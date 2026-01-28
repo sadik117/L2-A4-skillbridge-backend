131ea21219e7dc5da66a1515ef1ba97417a98773
unknown <sadiksourov11@gmail.com>
2026-01-29 00:17:30 +0600

    prisma setup done

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
+const port = process.env.PORT || 5000;
 
-const port = process.env.port || 3000;
+async function server() {
+  try {
+    await prisma.$connect();
 
-function server() {
+    app.listen(port, () => {
+      console.log(`Server is running at ${port}`);
+    });
 
-  app.listen(port, () => {
-    console.log(`Server is running at ${port}`);
-  });
-
+  } catch (error) {
+    console.log(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 }
 
 server();
