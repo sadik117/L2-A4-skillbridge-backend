4bac7ef7fc0697f80c66dab9e9c94b22b6c4f43e
unknown <sadiksourov11@gmail.com>
2026-01-30 11:14:31 +0600

    better auth setup done

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app";
+import { auth } from "./lib/auth";
 import { prisma } from "./lib/prisma";
+import { toNodeHandler } from "better-auth/node";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
+
+app.all("/api/auth/*", toNodeHandler(auth));
 
 async function server() {
   try {
