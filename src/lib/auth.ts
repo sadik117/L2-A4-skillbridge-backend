6fa747a4d231a413bc97c0a65fcd2f4eadcf5a3c
unknown <sadiksourov11@gmail.com>
2026-05-01 19:28:38 +0600

    google login added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -59,6 +59,13 @@ export const auth = betterAuth({
     minPasswordLength: 6,
   },
 
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+
   plugins: [
     admin({
       adminRoles: ["ADMIN"],
